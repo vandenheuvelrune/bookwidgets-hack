4f5491b62ffacc6261bb87a176af8f2c5201a3dc
Rune Van den Heuvel <53992897+DataM1nd@users.noreply.github.com>
2021-08-26 13:31:09 +0200

    Changed name and messages & added comments

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+// Insert the code below into the console when you selected the border of the blue 'send response' box using CTRL + SHIFT + C
 console.clear();
-console.group("%cBookwidgets hack", "font-size: 13px;", "color: aqua; font-size: 13px;");
+console.group("%cRetrieve bookwidget answers", "font-size: 13px;", "color: aqua; font-size: 13px;");
 try {
-    let studentAccount = "Pieter-Jan Hufkens";
-    let student = "Pieter-Jan Hufkens";
-    console.info("- Wijzigen van %cstudent account", "color: grey;");
+    let studentAccount = "Jean-Marie Baptiste";
+    let student = "Jean-Marie Baptiste";
+    console.info("- Wijzigen van %cstudent_account", "color: grey;");
     document.getElementById("student_account").style.display = "block";
     document.getElementById("student_account").value = studentAccount;
     document.getElementById("student_account").disabled = false;
-    console.info("- Wijzigen van %cstudent name", "color: grey;");
+    console.info("- Wijzigen van %cstudent_name", "color: grey;");
     document.getElementById("student_label").style.display = "block";
     document.getElementById("student").style.display = "block";
     document.getElementById("student").value = student;
     document.getElementById("student").disabled = false;
-    console.info("Hack %csuccesvol %cuitgevoerd", "color: green;", "color: ;")
+    console.info("Script %csuccesvol %cuitgevoerd", "color: green;", "color: ;")
     console.info("→ Druk op 'Versturen'")
-    console.groupEnd("%cBookwidgets hack");
+    console.groupEnd("%cetrieve bookwidget answers");
 } catch (err) {
-    console.error("Error when executing bookwidgets hack");
-    console.error("Try again later, error message:")
+    console.error("Er is een probleem opgetreden bij het uitvoeren van het script.");
+    console.error("Probeer het later nog eens opnieuw, error:")
     console.error(err.message);
-    console.groupEnd("%cBookwidgets hack");
+    console.groupEnd("%cRetrieve bookwidget answers");
 }
